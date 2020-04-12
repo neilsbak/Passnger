2bb4dd9cb3053a4497fd07670607817d75c122fe
Neil Bakhle <neilbakhle@gmail.com>
2020-04-12 12:31:09 -0400

    Initial Commit

---
+++ Passenger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Passenger
+//
+//  Created by Neil Bakhle on 2020-04-12.
+//  Copyright © 2020 Neil. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
